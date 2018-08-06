fb02cee866935344bef55d0ff80e654b8f991161
nari19 <kazuki7793996@gmail.com>
2018-08-06 18:43:12 +0900

    edit readme

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
 registerServiceWorker();
+
+
+// create-react-appを使ってReactコンポーネントの素振り、GitHub Pagesへのデプロイまで
+// https://blog.wadackel.me/2016/create-react-app/
