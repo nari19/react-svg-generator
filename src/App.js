84239488da0e1fc7703d006a33871686dfb53156
nari19 <kazuki7793996@gmail.com>
2018-08-06 19:27:50 +0900

    appjs

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class List extends Component {
   // 書き込み一覧をサーバーから取得
   componentDidMount() {
     fetch('/api/v1/Post?sort={"_id":-1}')
-      .then(response => response.json())
-      .then(date => this.setState({posts: date}));
+      // .then(response => response.json())
+      // .then(date => this.setState({posts: data }));
   }
   render(){
     const {posts} = this.state;
@@ -35,20 +35,38 @@ class List extends Component {
   }
 }
 
-
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>掲示板</h1>
+        <nav>
+          <ul>
+            <li><a href="/">トップ</a></li>
+            <li><a href="#">投稿</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
 
 
 class App extends Component {
   render() {
     return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <List />
       </div>
     );
   }
