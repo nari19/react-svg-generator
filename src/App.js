e7cd9d81e2cc214a4fdaf90f6d06dba7225312c0
nari19 <kazuki7793996@gmail.com>
2018-08-10 10:29:02 +0900

    monaco-editor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MonacoEditor from 'react-monaco-editor';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+    }
+  }
+  editDidMount(editor, monaco) {
+    console.log('editorDidmount', editor);
+    editor.focus();
+  }
+  onChange(newValue, e) {
+    console.log('onChange', newValue, e);
+  }
   render() {
+    const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MonacoEditor
+        width="800"
+        height="600"
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        // onChange={::this.onChange}
+        // editorDidMount={::this.editorDidMount}
+      />
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
